eb7ca7de4747da8acd459e9306143fdf24486091
Evan Paul Jacob <evanpauljacob27@gmail.com>
2026-06-14 00:53:12 +0530

    Add script to fetch and save GitHub repositories
    Fetches user repositories from GitHub API and saves non-forked projects to a JSON file.

--- fetch_repository.py
+++ fetch_repository.py
@@ -1,1 +1,26 @@
+import json
+import requests
 
+USERNAME = "evanpaul27"
+
+url = f"https://api.github.com/users/{USERNAME}/repos"
+
+repos = requests.get(url).json()
+
+projects = []
+
+for repo in repos:
+
+    if repo["fork"]:
+        continue
+
+    projects.append({
+        "name": repo["name"],
+        "description": repo["description"] or "No description provided",
+        "url": repo["html_url"]
+    })
+
+with open("projects.json", "w") as f:
+    json.dump(projects, f, indent=4)
+
+print("projects.json updated")
